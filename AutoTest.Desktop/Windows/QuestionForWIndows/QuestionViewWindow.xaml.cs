003b448c095c137325869edf09177dc179147aba
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-01-17 23:50:19 +0500

    Added QuestionViewWindow & Component

---
+++ AutoTest.Desktop/Windows/QuestionForWIndows/QuestionViewWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using AutoTest.Desktop.Components.QuestionForComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AutoTest.Desktop.Windows.QuestionForWIndows
+{
+    /// <summary>
+    /// Interaction logic for QuestionViewWindow.xaml
+    /// </summary>
+    public partial class QuestionViewWindow : Window
+    {
+        public QuestionViewWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            stQuestions.Children.Clear();
+
+            for(int i = 0; i < 10; i++)
+            {
+                QuestionComponent component = new QuestionComponent();
+                stQuestions.Children.Add(component);
+            }
+        }
+    }
+}
