4b147b0d68a8e4f24bd2f8d7c3f71a556a429bd0
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-02-12 00:51:57 +0500

    Add UpdateOptionPage and enhance option handling
    - Remove unused `using` directives and hide `EmptyData` in `CreateOptionPage.xaml.cs`
    - Add `using AutoTest.Domain.Enums` and new `Get` method in `UpdateQuestionWindow.xaml.cs`
    - Create `UpdateOptionPage.xaml` with layout for input fields and options
    - Add `UpdateOptionPage.xaml.cs` with necessary `using` directives and constructor

--- AutoTest.Desktop/Pages/OptionForPage/CreateOptionPage.xaml.cs
+++ AutoTest.Desktop/Pages/OptionForPage/CreateOptionPage.xaml.cs
@@ -1,20 +1,7 @@
 ﻿using AutoTest.BLL.DTOs.Tests.Option;
 using AutoTest.Desktop.Components.OptionForComponents;
-using AutoTest.Domain.Entities.Tests;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using ToastNotifications;
 using ToastNotifications.Lifetime;
 using ToastNotifications.Messages;
@@ -69,6 +56,7 @@ namespace AutoTest.Desktop.Pages.OptionForPage
         {
             if (questionOptions.Any())
             {
+                EmptyData.Visibility = Visibility.Collapsed;
                 foreach (var option in questionOptions)
                 {
                     options.Add(option);
