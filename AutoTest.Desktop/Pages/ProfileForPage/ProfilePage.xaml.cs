7cf623f9e427ae699c8f0660360dd5dea45517ed
foziljonovs <abdulvosidfoziljonov55@gmail.com>
2025-02-19 00:06:09 +0500

    Update ProfilePage UI and logic for test count display
    Updated ProfilePage.xaml to modify UI elements, including changing initial content of test count labels and adding new labels for user information. Updated ProfilePage.xaml.cs to refactor imports, introduce a test service, and add methods to retrieve and display user test counts and full name.

--- AutoTest.Desktop/Pages/ProfileForPage/ProfilePage.xaml.cs
+++ AutoTest.Desktop/Pages/ProfileForPage/ProfilePage.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using AutoTest.Desktop.Integrated.Security;
-using AutoTest.Domain.Entities.Users;
+using AutoTest.Desktop.Integrated.Services.Test;
 using System.Windows.Controls;
 
 namespace AutoTest.Desktop.Pages.ProfileForPage
@@ -9,14 +9,45 @@ namespace AutoTest.Desktop.Pages.ProfileForPage
     /// </summary>
     public partial class ProfilePage : Page
     {
+        private readonly ITestService _testService;
+        private long userId { get; set; }
         public ProfilePage()
         {
             InitializeComponent();
+            this._testService = new TestService();
         }
 
         private void Page_Loaded(object sender, System.Windows.RoutedEventArgs e)
         {
-            ProfileLbl.Name = GetUser();
+            GetAll();
+        }
+
+        private async void GetAll()
+        {
+            userId = IdentitySingelton.GetInstance().Id;
+            var fullName = GetUser();
+            if (fullName is not null)
+                UserFullNameLbl.Content = fullName;
+            else
+                UserFullNameLbl.Content = "...";
+
+            var count = await GetTestsCount(userId);
+            if (count > 0)
+                YourTestCount.Content = count.ToString();
+            else
+            {
+                YourTestCount.Content = "0";
+                YourTestCountText.Content = "Testlar mavjud emas";
+            }
+        }
+
+        private async Task<int> GetTestsCount(long userId)
+        {
+            var tests = await _testService.GetAllByUserIdAsync(userId);
+            if (!tests.Any())
+                return 0;
+
+            return tests.Count();
         }
 
         private string GetUser()
