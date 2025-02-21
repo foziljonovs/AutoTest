2f276b95c36bb64c38234fdcd953cdc9b4498d5c
Abdulvosid Foziljonov <89521523+foziljonovs@users.noreply.github.com>
2025-02-21 18:07:12 +0500

    Add new methods to IUserServer interface
    Updated the IUserServer interface in IUserServer.cs to include five new asynchronous methods: UpdateAsync, DeleteAsync, ChangePasswordAsync, and VerifyPasswordAsync. These methods facilitate user information updates, user deletions, password changes, and password verification, each returning a Task<bool> to indicate success.

--- AutoTest.Desktop.Integrated/Servers/Interfaces/User/IUserServer.cs
+++ AutoTest.Desktop.Integrated/Servers/Interfaces/User/IUserServer.cs
@@ -6,4 +6,8 @@ public interface IUserServer
 {
     Task<List<UserDto>> GetAllAsync();
     Task<UserDto> GetByIdAsync(long id);
+    Task<bool> UpdateAsync(long id, UpdateUserDto dto);
+    Task<bool> DeleteAsync(long id);
+    Task<bool> ChangePasswordAsync(long id, UserChangePasswordDto dto);
+    Task<bool> VerifyPasswordAsync(long id, string password);
 }
